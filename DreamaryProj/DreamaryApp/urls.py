a21b6785feb83cd8fe69cb2f439ce0620727d5f0
sion <aabbcc030421@gamil.com>
2020-08-19 18:48:22 +0900

    add introduce & update

--- DreamaryProj/DreamaryApp/urls.py
+++ DreamaryProj/DreamaryApp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('create/', views.create, name="create"),
+    path('introduce/', views.introduce, name="introduce"),
+    path('update/<int:designer_id>/', views.update, name="update"),
 ]
--- DreamaryProj/DreamaryApp/views.py
+++ DreamaryProj/DreamaryApp/views.py
@@ -5,6 +5,9 @@ from .models import Designer
 def home(request):
     return render(request, 'home.html')
 
+def introduce(request):
+    return render(request, 'introduce.html')
+
 
 def create(request):
     if request.method == 'POST':
@@ -17,3 +20,19 @@ def create(request):
         return redirect('home')
     else:
         return render(request, 'new.html')
+
+def update(request, designer_id):
+    get_object_or_404(Designer, pk = designer_id)
+
+    if request.method == 'POST':
+        if 'image' in request.FILES:
+            post.image = request.FILES['image']
+        post.name = request.POST['name']
+        post.address = request.POST['address']
+        post.description = request.POST['description']
+
+        post.save()
+
+        return redirect('detail', post.id)
+    else:
+        return render(request, 'update.html', {'designer' : post})
