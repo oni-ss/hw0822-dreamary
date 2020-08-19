66bcc5dc0d0ab423e9c1c22c72bbe44f2d070e9f
sion <aabbcc030421@gamil.com>
2020-08-19 19:41:48 +0900

    수정

--- DreamaryProj/DreamaryApp/urls.py
+++ DreamaryProj/DreamaryApp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('introduce/', views.introduce, name="introduce"),
     path('update/<int:designer_id>/', views.update, name="update"),
     path('detail/<int:pk>', views.detail, name="detail"),
-     path('delete/<int:pk>', views.delete, name="delete"),
+    path('delete/<int:pk>', views.delete, name="delete"),
 ]
