a6d7060d6988b96c4f2b827af01c9f2e05d67105
oni <sion0210@naver.com>
2020-08-19 18:01:29 +0900

    사진 해결

--- DreamaryProj/DreamaryProj/settings.py
+++ DreamaryProj/DreamaryProj/settings.py
@@ -111,3 +111,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'DreamaryApp', 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
