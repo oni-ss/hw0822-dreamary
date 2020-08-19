6584201bca1d4bf666565fc116f8722f932faf5e
hyosunLee <leecrossun@gmail.com>
2020-08-19 18:07:15 +0900

    delete

--- DreamaryProj/DreamaryApp/urls.py
+++ DreamaryProj/DreamaryApp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('create/', views.create, name="create"),
     path('detail/<int:pk>', views.detail, name="detail"),
+     path('delete/<int:pk>', views.delete, name="delete"),
 ]
